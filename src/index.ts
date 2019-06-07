37bd1ff960f6a5f6505843c15a2fdc5f9b49155b
Garth Williams <garth@wi.llia.ms>
2019-06-07 15:42:57 +0200

    Fix es5 build

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export interface FormDefinition {
   [fieldName: string]: {
     value?: string
     isValid?: IsValid
+    isPristine?: boolean
   }
 }
 export type FormFields<T extends FormDefinition> = { [F in keyof T]: FormField }
@@ -19,20 +20,34 @@ export type Reset<T extends FormDefinition> = (formDefinition?: FormJson<T>) => 
 export type FormJson<T extends FormDefinition> = { [F in keyof T]: string }
 
 const validateForm = <T extends FormDefinition>(formDefinition: T): Form<T> =>
-  Object.keys(formDefinition).reduce<Form<T>>(
+  Object.keys(formDefinition).reduce(
     (form, fieldName) => {
       const field = formDefinition[fieldName]
-      const value = field.value || ''
+      const value = field.value
       const isValid = typeof field.isValid === 'function' ? field.isValid(value, formDefinition) : true
+      const isPristine = field.isPristine
       form[fieldName] = {
         value,
-        isPristine: field['isPristine'] === undefined ? true : field['isPristine'],
+        isPristine,
         isValid
       }
       form.isValid = form.isValid && isValid
       return form
     },
-    { isValid: true } as any
+    { isValid: true } as Form<T>
+  )
+
+const setFormValues = <T extends FormDefinition>(definition: T, values?: FormJson<T>): T =>
+  Object.keys(definition).reduce(
+    (form, field) => {
+      form[field] = {
+        ...definition[name],
+        isPristine: true,
+        value: (values && values[name]) || definition[name].value || ''
+      }
+      return form
+    },
+    {} as T
   )
 
 export const useForm = <T extends FormDefinition>(
@@ -44,23 +59,11 @@ export const useForm = <T extends FormDefinition>(
   reset: Reset<T>
   formToJson: (form: Form<T>) => FormJson<T>
 } => {
-  const setFormValues = (formDefinition: T, formValues?: FormJson<T>) =>
-    Object.keys(formDefinition).reduce(
-      (form, field) => {
-        form[field] = {
-          ...formDefinition[name],
-          value: (formValues && formValues[name]) || formDefinition[name].value || ''
-        }
-        return form
-      },
-      {} as T
-    )
-
   const [originalFormDefinition] = useState(formDefinition)
   const [originalFormValues] = useState(formValues)
-  const [form, setForm] = useState(() => setFormValues(formDefinition))
+  const [form, setForm] = useState(formDefinition)
 
-  const setField = (name: string, value: string) => {
+  const setField = (name: keyof T, value: string) => {
     setForm({
       ...form,
       [name]: {
